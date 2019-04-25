887297fc65e010746655905162a1b6dc5666ebb2
Gilbishkosma <gilbeeshkosma15@gmail.com>
2019-04-26 00:21:49 +0530

    Finally Completed

--- src/App.js
+++ src/App.js
@@ -12,18 +12,34 @@ import meow from './img/meowth.svg';
 import pidgey from './img/pidgey.svg';
 import squirtle from './img/squirtle.svg';
 import pokeball from './img/pokeball.svg';
+import snorlax from './img/snorlax.svg';
+import jigglypuff from './img/jigglypuff.svg';
+import mankey from './img/mankey.svg';
+import duck from './img/psyduck.svg';
+import stardust from './img/stardust.svg';
+import tornado from './img/tornado.svg';
+import weedle from './img/weedle.svg';
+import zubat from './img/zubat.svg';
+
 import './App.css';
 
 
 const pokemons = [{id:1,'img':abra,'name':'Abra','price':'30','tags':'fight'}
                  ,{id:2,'img':bellsprout,'name':'Bellsprout','price':'60','tags':'plant'},
-                 ,{id:3,'img':bullbasaur,'name':'Bullbasaur','price':'90','tags':'fight'},
+                 ,{id:3,'img':bullbasaur,'name':'Bullbasaur','price':'90','tags':'plant'},
                  {id:4,'img':caterpie,'name':'Caterpie','price':'70','tags':'plant'},
                 {id:5,'img':charmander,'name':'Charmander','price':'40','tags':'fire'},
                 {id:6,'img':pikachu,'name':'Pikachu','price':'150','tags':'electric'},
                 {id:7,'img':meow,'name':'Meowth','price':'0','tags':'normal'},
                 {id:8,'img':pidgey,'name':'Pidgey','price':'20','tags':'bug'},
-                {id:9,'img':squirtle,'name':'Squirtle','price':'50','tags':'water'}
+                {id:9,'img':squirtle,'name':'Squirtle','price':'50','tags':'water'},
+                {id:10,'img':snorlax,name:'Snorlax',price:'60',tags:'water'},
+                {id:11,img:jigglypuff,name:'Jigglypuff',price:'10',tags:'normal'},
+                {id:12,img:mankey,name:'Mankey',price:'30',tags:'fight'},
+                {id:13,img:duck,name:'Psyduck',price:'20',tags:'normal'},
+                {id:14,img:tornado,name:'Tornado',price:'50',tags:'fight'},
+                {id:15,img:weedle,name:'Weedle',price:'80',tags:'bug'},
+                {id:16,img:zubat,name:'Zubat',price:'30',tags:'fight'}
                 ]
 var copypoke = [...pokemons]
 var cart = []
@@ -31,9 +47,13 @@ const tags = [{id:1,tag:'All'},{id:2,tag:'Normal'},{id:3,tag:'fight'},{id:4,tag:
 
 
 function Tags(props){
-  const {tag,searchmethod} = props
+  const {tag,searchmethod,tags,selected_tag} = props
+  const selectedTag = selected_tag.tag === tag
+  // const tagActivated  = tags.filter(item => item.tag === tag)
   return (
-    <button className="button is-rounded"  style={{marginRight : '20px'}} onClick={() => searchmethod(tag)}>{tag}</button>
+    <div class="column">
+    <button className={`${selectedTag === true ? 'button is-rounded is-primary' : 'button is-rounded'}`}  style={{marginRight : '20px'}} onClick={() => searchmethod(tag)}>{tag}</button>
+    </div>
     )
 }
 
@@ -42,7 +62,7 @@ const {item,removepoke} = props
 return (
  <div className="column is-gapless">
  <button id={item.img} className="delete is-small" onClick={() => removepoke(item)} />
- <img src={item.img} style={{maxWidth:'70px',minWidth:'40px'}}/>
+ <img src={item.img} style={{maxWidth:'70px'}}/>
  </div>
   )
 }
@@ -53,12 +73,12 @@ const {cartvisible,hidecart,removepoke} = props
 return(
    <div className={`${cartvisible === true ? '' : 'is-invisible'}`}>
    <section className="hero is-primary fix-footer">
-  <div className="tags has-addons is-right" style={{padding:'10px'}}>
+  <div className="tags has-addons is-right" style={{padding:'2px'}}>
   <span className="tag is-danger">Close</span>
   <a className="tag is-delete" onClick={hidecart}></a>
 </div>
 <div className="hero-body">
-<div className="columns is-multiline">
+<div className="columns is-multiline is-mobile">
 {cart.map((item) => <Cartimages item={item} key={item.id} removepoke={removepoke} />) }
 </div>
 </div>
@@ -72,9 +92,10 @@ class MyCard extends Component{
       super(props)
     }
     render(){
-      const {img,name,price,addcart,id} = this.props
+      const {img,name,price,addcart,id,cart} = this.props
+      const cardExist = cart.filter((item) => item.id === id ).length > 0;
       return(
-        <div>
+        <div className="is-centered">
         <div className="card" style={{width:'220px'}}>
         <div className="card-image" style={{paddingTop:'10px',paddingLeft:'25px',paddingRight:'25px',paddingBottom:'20px'}}>
         <img src={img} />
@@ -82,7 +103,7 @@ class MyCard extends Component{
         <h1 className="title is-4 has-text-centered">{name}</h1>
         <h1 className="subtitle is-4 has-text-centered has-text-success">${price}</h1>
         <div className="has-text-centered" style={{ paddingBottom:'15px',paddingLeft:'10px',paddingRight:'10px',paddingTop:'0px'}}>
-        <button className="button is-fullwidth" onClick={() => addcart(id)}> Add to Cart</button>
+        <button className={`${cardExist === true ? 'button is-fullwidth is-info' : 'button is-fullwidth'}`}  onClick={() => addcart(id)} disabled={cardExist}>{cardExist ? 'Added' : 'Add to cart'}</button>
         </div>
         </div>
         </div>
@@ -97,10 +118,10 @@ class CardList extends Component{
     super(props);
  }
  render(){
-   const {img,name,price,addcart,id} = this.props
+   const {img,name,price,addcart,id,cart} = this.props
    return(
-    <div className="column is-3" >
-      <MyCard img={img}  name={name} price={price} addcart={addcart} id={id}/>
+    <div className="column is-fullwidth" >
+      <MyCard img={img}  name={name} price={price} addcart={addcart} cart={cart} id={id}/>
     </div>
    )   
  }
@@ -129,7 +150,7 @@ function tagsearch(searchterm){
 class PokeCard extends Component{
   constructor(props){
      super(props);
-     this.state = {results:this.props.pokemons,search:'',result_tags:this.props.tags,pokecart:this.props.cart,cartvisible:false}
+     this.state = {results:this.props.pokemons,search:'',result_tags:this.props.tags,selected_tag:tags[0],pokecart:this.props.cart,cartvisible:false}
      this.searchchange = this.searchchange.bind(this);
      this.showtags = this.showtags.bind(this);
      this.addcart = this.addcart.bind(this);
@@ -185,14 +206,17 @@ class PokeCard extends Component{
       copypoke = pokemons
       var newresult = pokemons
       this.setState({
-        results:newresult
+        results:newresult,
+        selected_tag:tags[0]
          })
     }
     else{
        var newresult = pokemons.filter(tagsearch(tag))
+       var selected = tags.filter(item => item.tag == tag)[0]
        copypoke = newresult
        this.setState({
-          results:newresult
+          results:newresult,
+          selected_tag:selected
        })
     }
   }
@@ -200,24 +224,29 @@ class PokeCard extends Component{
   render(){
     var results = this.state.results
     return(
-      <div style={{marginBottom:'200px'}}>
+      
+      <div style={{marginBottom:'300px'}}>
+      <section className="section" style={{marginRight:'30px'}}>
       <div className="container">
       <div className="has-text-centered" style={{marginBottom:'10px'}}>
       <img src={pokeball} style={{maxWidth:'70px'}}/>
       </div>
-      <div className="has-text-right">
+      <div className="has-text-right is-flex-mobile">
       <img src={cartlogo} style={{maxWidth:'30px'}} onClick={this.showcart} />
       {cart.length}
       </div>
+      <br/>
+      <div className="control">
       <input className="input" type="text" value={this.state.search} placeholder="Gotta Catch'em all" style={{marginBottom : '10px'}} onChange={this.searchchange} />
-
-      <div style={{marginBottom:'20px'}}>
-      {tags.map((item) => <Tags tag={item.tag} key={item.id.toString()} searchmethod={this.showtags}/> )}
       </div>
-      <div className="columns is-multiline">
-      {results.map((item) => <CardList img={item.img} name={item.name} price={item.price} addcart={this.addcart} id={item.id} key={item.id.toString()} />)}
+      <div className="columns is-multiline is-mobile" style={{marginBottom:'20px'}}>
+      {tags.map((item) => <Tags tag={item.tag} tags={tags} key={item.id.toString()} selected_tag={this.state.selected_tag} searchmethod={this.showtags}/> )}
+      </div>
+      <div className="columns is-multiline is-mobile">
+      {results.map((item) => <CardList img={item.img} cart={this.state.pokecart} name={item.name} price={item.price} addcart={this.addcart} id={item.id} key={item.id.toString()} />)}
       </div>
       </div>
+      </section>
       <Cartshow cartvisible={this.state.cartvisible} hidecart={this.hidecart} removepoke={this.removepoke}/>
       </div>
     )
@@ -228,8 +257,14 @@ class PokeCard extends Component{
 class App extends Component {
   render() {
     return (
-    <div style={{marginTop:'20px'}}>
+    <div style={{marginTop:'20px'}}> 
     <PokeCard pokemons={pokemons} tags={tags} cart={cart} />
+    <footer class="footer">
+    <div class="has-text-right" style={{marginRight:"40px"}}> 
+    <p>Made by <a href="https://github.com/Gilbishkosma/">Gilbish kosma</a>. 
+    An assignment of <a href="https://reactjs.org">React</a> from <a href="https://hackr.io/hack-n-learn/">hackr.io</a>.</p>
+    </div>
+    </footer>
     </div>
     );
   }
